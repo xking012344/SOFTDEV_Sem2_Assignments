d58acaa8dd27f948e5f0409ce68cff6d2454cb57
Xavier Harding <hardixav000@warren.k12.in.us>
2024-01-11 10:39:39 -0500

    tech n9ne

--- 04_Try Catch Finally/Assignment/src/App.java
+++ 04_Try Catch Finally/Assignment/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int firstNumber = input.nextInt();
+        System.out.println("Enter another number: ");
+        int secondNumber = input.nextInt();
+        
+        int quotient = 0;
+        try{
+            quotient = firstNumber / secondNumber;
+        }catch(ArithmeticException ex){
+            System.out.println(ex.getMessage());
+            System.out.println("Default answer of 0 used for quotient");
+        }finally{
+            System.out.println(String.format("The answer from division is %s", quotient));
+        }
+        input.close();
     }
 }
