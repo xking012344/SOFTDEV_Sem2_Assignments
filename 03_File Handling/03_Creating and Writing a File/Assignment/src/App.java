b270d7a741d54a4fe97b6862126ab6fcf3f5a556
Xavier Harding <hardixav000@warren.k12.in.us>
2024-02-02 10:24:30 -0500

    dizzy

--- 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
+++ 03_File Handling/03_Creating and Writing a File/Assignment/src/App.java
@@ -1,5 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String FILE_NAME = "src/starter.txt";
+        String newFile = "src/ender.txt";
+
+        Path filePath = Paths.get(FILE_NAME);
+        List<String> lines = Files.readAllLines(filePath);
+
+        // create file using notes from yesterday 53-55
+        FileWriter fileWriter = new FileWriter(newFile, true);
+        BufferedWriter writer = new BufferedWriter(fileWriter);
+
+        for (var line : lines) {
+            // write line to file using append in the notes
+            writer.append(line);
+            writer.append("\n");
+
+        }
+        writer.close();
+
     }
 }
