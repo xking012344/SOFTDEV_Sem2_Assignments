b07c04954cf5d98c8355e77ca2d424026736b5ce
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-02-06 10:41:43 -0500

    updates

--- 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
+++ 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
@@ -1,5 +1,45 @@
+import java.util.function.Function;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double cFromOO = lawOfCosinesOO(1, 1, 90);
+        double cFromFPDumb = lawOfCosineFPDumb.apply(1.0, 1.0, 90.0);
+        var cFromFPSmart = lawOfCosinesSmart.apply(1.0).apply(1.0);
+        System.out.println(cFromOO);
+        System.out.println(cFromFPDumb);
+        System.out.println(cFromFPSmart);
     }
+
+    public static double lawOfCosinesOO(double a, double b, double angDegrees) {
+        double angRadians = Math.toRadians(angDegrees);
+        double cosinePart = 2 * a * b * Math.cos(angRadians);
+        double aSq = a * a;
+        double bSq = b * b;
+        return Math.sqrt(aSq + bSq - cosinePart);
+    }
+
+    public static TriFunction<Double, Double, Double, Double> lawOfCosineFPDumb = (a, b, angDegrees) -> {
+        double angRadians = Math.toRadians(angDegrees);
+        double cosinePart = 2 * a * b * Math.cos(angRadians);
+        double aSq = a * a;
+        double bSq = b * b;
+        return Math.sqrt(aSq + bSq - cosinePart);
+    };
+    public static Function<Double, Function<Double, Function<Double, Double>>> lawOfCosinesSmart = (
+            a) -> (b) -> (angDegrees) -> {
+                double angRadians = Math.toRadians(angDegrees);
+                double cosinePart = 2 * a * b * Math.cos(angRadians);
+                double aSq = a * a;
+                double bSq = b * b;
+                return Math.sqrt(aSq + bSq - cosinePart);
+            };
+
+    // TODO: write a function called lawOfCosinesAngle
+    public static Function<Double, Function<Double, Function<Double, Double>>> lawOfCosinesAngleC = (
+            a) -> (b) -> (c) -> {
+
+                //angleC = acos((c^2 - a^2 - b^2) / (-2ab))
+
+            };
+
 }
