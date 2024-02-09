23d1fbcb708f4416fe24e2195fb3600fb6980657
Xavier Harding <hardixav000@warren.k12.in.us>
2024-02-09 10:48:50 -0500

    df

--- 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
+++ 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
@@ -3,21 +3,25 @@ import java.util.function.Function;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //TODO:  make a function called subtract5 using lambda notation
-        //TODO: make a function called times5 using lambda notation
-        //TODO: make a function called subtract5Times5 using andThen
+        // TODO: make a function called subtract5 using lambda notation
+        Function<Integer, Integer> subtract5 = (num) -> num - 5;
+        // TODO: make a function called times5 using lambda notation
+        Function<Integer, Integer> times5 = (num) -> num * 5;
+        // TODO: make a function called subtract5Times5 using andThen
+        Function<Integer, Integer> subtract5Times5 = subtract5.andThen(times5);
 
-        //TODO:  create an int called testNumber set to 1;
-        //TODO:  apply subtract5Times5 to testNumber
+        // TODO: create an int called testNumber set to 1;
+        Integer testNumber = 1;
+        // TODO: apply subtract5Times5 to testNumber
+        Integer testNumberResult = subtract5Times5.apply(testNumber);
 
-
-        //Lambda notation examples
+        // Lambda notation examples
         Function<Integer, Integer> add5 = (num) -> num + 5;
         Function<Integer, Integer> div2 = (num) -> num / 2;
         // Function<Integer, Integer> add5Divide2 = (num) -> {
-        //     num = num + 5;
-        //     num = num / 5;
-        //     return num;
+        // num = num + 5;
+        // num = num / 5;
+        // return num;
         // };
 
         Function<Integer, Integer> add5Divide2UsingAndThen = add5.andThen(div2);
