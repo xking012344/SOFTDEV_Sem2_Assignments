1113e89359375394cc1249a14fd2de92fe45b235
Xavier Harding <hardixav000@warren.k12.in.us>
2024-03-01 10:32:35 -0500

    king

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,5 +1,15 @@
+import java.util.concurrent.CompletableFuture;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CompletableFuture<Double> ranNum = CompletableFuture.supplyAsync(
+            () -> Math.random()
+        );
+
+        CompletableFuture<Void> newRanNum = ranNum.thenAccept(s -> {
+            System.out.println(s * 2);
+        });
+
+        newRanNum.get();
     }
 }
