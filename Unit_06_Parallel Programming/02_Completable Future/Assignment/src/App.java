1de715d5244a9cf685c2d47bcb6bb19f56ed0fb4
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-03-04 10:42:51 -0500

    updates

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,5 +1,18 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        BiFunction<Integer, Integer, Integer> randoGenerator = (min, max) -> {
+            int size = max - min + 1;
+            double rando = size * Math.random();
+            return (int) Math.floor(rando);
+        };
+
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> randoGenerator.apply(0, 10));
+
+        System.out.println(future.get());
     }
+
 }
