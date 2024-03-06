c8fdf4e295d1b28a245b4ff001dfce7749f0b15b
Xavier Harding <hardixav000@warren.k12.in.us>
2024-03-06 10:22:31 -0500

    fix

--- Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
+++ Unit_06_Parallel Programming/02_Completable Future/Assignment/src/App.java
@@ -1,12 +1,10 @@
 import java.util.concurrent.CompletableFuture;
-<<<<<<< HEAD
 
 public class App {
     public static void main(String[] args) throws Exception {
         CompletableFuture<Double> ranNum = CompletableFuture.supplyAsync(
             () -> Math.random()
         );
-
         CompletableFuture<Void> newRanNum = ranNum.thenAccept(s -> {
             System.out.println(s * 2);
         });
@@ -14,22 +12,3 @@ public class App {
         newRanNum.get();
     }
 }
-=======
-import java.util.function.BiFunction;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-
-        BiFunction<Integer, Integer, Integer> randoGenerator = (min, max) -> {
-            int size = max - min + 1;
-            double rando = size * Math.random();
-            return (int) Math.floor(rando);
-        };
-
-        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> randoGenerator.apply(0, 10));
-
-        System.out.println(future.get());
-    }
-
-}
->>>>>>> 1de715d5244a9cf685c2d47bcb6bb19f56ed0fb4
